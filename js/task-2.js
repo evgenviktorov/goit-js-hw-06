e3355a890df37607acf44aaac67986f37a744ad9
EV <evgenviktorov88@gmail.com>
2025-04-14 22:20:31 +0300

    init

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,14 @@
 console.log('-- task-2 --');
 
 // ==============
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,11 @@
 console.log('-- task-3 --');
 
 // ==============
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
